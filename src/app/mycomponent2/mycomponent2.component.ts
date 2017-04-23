e58512123c33e0954354a69abaffb736fb4dfaf7
Gaurav Sharma <gaurav.v.sharma@gmail.com>
2017-04-23 14:27:06 +0530

    tab interface code

--- src/app/mycomponent2/mycomponent2.component.ts
+++ src/app/mycomponent2/mycomponent2.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Mycomponent2Component implements OnInit {
 
-  constructor() { }
+  tabs:number;
+
+  constructor() { 
+    this.tabs = 5;
+  }
 
   ngOnInit() {
+    var allTabs = document.querySelectorAll('.tm-tab-content-box');
+    var i;
+    for (i = 0; i < allTabs.length; i++) {
+      allTabs[i].classList.add('sr-only');
+    }
+  }
+
+  showTab(tabId: string) {
+    var t = tabId;
+    console.log(t);
+
+    document.querySelector('.tm-tab-content-box').classList.add('sr-only');
+
+    document.querySelector('#' + t).classList.remove('sr-only');
+    document.querySelector('#' + t).classList.add('show');
   }
 
 }
